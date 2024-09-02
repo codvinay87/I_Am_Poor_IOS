6e62420439108d4321f6ac6df77fc362b468ce61
codvinay <vinay11gemini@gmail.com>
2024-09-02 07:52:42 +0530

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by VINAY SINGHANIA on 02/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
